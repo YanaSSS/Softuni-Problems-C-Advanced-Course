44fd5a3ec342af43deadf885b9f4747980567613
YanaSSS <43813983+YanaSSS@users.noreply.github.com>
2019-10-01 08:46:37 +0300

    Add files via upload

---
+++ Cups and Bottles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList<int> cups = new LinkedList<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            Stack<int> bottles = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+
+            int wastedWater = 0;
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                int currentBottle = bottles.Pop();
+                if (cups.First() < currentBottle)
+                {
+                    wastedWater += (currentBottle - cups.First());
+                    cups.RemoveFirst();
+                }
+                
+                else
+                {
+                    var firstCup = cups.First();
+                    cups.RemoveFirst();
+                    firstCup -= currentBottle;
+                    cups.AddFirst(firstCup);
+                }
+
+                if (cups.Count>0 && cups.First() == 0)
+                {
+                    cups.RemoveFirst();
+
+                }
+            }
+
+            if(cups.Count==0)
+            {
+                Console.WriteLine("Bottles: " + string.Join(' ', bottles));
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+            else if(bottles.Count==0)
+            {
+                Console.WriteLine("Cups: " + string.Join(' ', cups));
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+        }
+    }
+}
---
+++ Key Revolver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Key_Revolver
+{
+    class Program
+    {
+        static void Main()
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] locks = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int intelligenceValue = int.Parse(Console.ReadLine());
+
+            Stack<int> loadedGun = new Stack<int>(bullets);
+            Queue<int> locksToBreak = new Queue<int>(locks);
+
+            int bulletsUsed = 0;
+
+            while (loadedGun.Count != 0 && locksToBreak.Count != 0)
+            {
+
+
+                int currentBullet = loadedGun.Pop();
+
+                if (currentBullet <= locksToBreak.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locksToBreak.Dequeue();
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                bulletsUsed++;
+
+                if (bulletsUsed % gunBarrelSize == 0 && loadedGun.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                }
+
+            }
+
+            if (locksToBreak.Count != 0)
+            {
+
+                Console.WriteLine($"Couldn't get through. Locks left: {locksToBreak.Count}");
+
+            }
+            else if (locksToBreak.Count == 0)
+            {
+                Console.WriteLine($"{loadedGun.Count} bullets left. " +
+                    $"Earned ${ intelligenceValue - (bulletsUsed * bulletPrice)}");
+            }
+        }
+    }
+}
